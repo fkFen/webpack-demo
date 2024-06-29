c037635613fdbd04bb19e05728776df993672dcc
fkf <2267442380@qq.com>
2024-06-29 10:30:34 +0800

    新建项目

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import data from "./data.json"
+import "./index.css"
+import "./index.less"
+console.log(data)
+function fn(x,y){
+    return x+y
+}
+console.log(fn(3,5))
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const {resolve}=require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+module.exports={
+    entry:"./src/index.js",
+    output:{
+        filename:"main.js",
+        path:resolve(__dirname,"dist")
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test:/\.less$/,
+                use:[
+                    "style-loader",
+                    "css-loader",
+                    "less-loader"
+                ]
+            },
+            {
+                test:/\.html$/,
+                use:["html-loader"]
+            }
+        ]
+    },
+    //插件
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./src/index.html'
+        })
+    ],
+    mode:'development',
+    devServer:{
+        port:4000
+    }
+}
